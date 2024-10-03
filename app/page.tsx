139668ccb11331eec4c0cfca612fb3fef3b22d56
ankitsjadhav <ankitsj7@gmail.com>
2024-10-03 20:36:56 +0530

    Testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
     return (
+        <div className="flex flex-col">
         <p className="text-3xl font-bold text-indigo-500">
             Hello Discord
         </p>
+        <Button variant="test">
+            Click Me
+        </Button>
+        </div>
     )
 }
